596d2d22df71034a3e044ad44eb05f043d5f33a6
Deepak Khatri <dk794453@gmail.com>
2026-01-06 21:49:52 +0530

    Add files via upload

---
+++ Part1/anecdotes/src/App.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react'
+
+const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...',
+    'Any fool can write code that a computer can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code.',
+    'Programming without console.log is like a doctor without tests.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  
+  const nextAnecdote = () => {
+    const random = Math.floor(Math.random() * anecdotes.length) 
+    setSelected(random)
+  }
+  
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const maxVotes = Math.max(...votes)
+  const topIndex = votes.indexOf(maxVotes)
+
+
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+
+      <button onClick={vote}>vote</button>
+      <button onClick={nextAnecdote}>next anecdote</button>
+
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[topIndex]}</p>
+      <p>has {maxVotes} votes</p>
+    </div>
+  )
+}
+
+export default App
