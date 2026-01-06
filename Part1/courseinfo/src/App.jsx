dfdaf5ef48fd903fe4b28368898e8ee724016046
Deepak Khatri <dk794453@gmail.com>
2026-01-06 21:57:06 +0530

    Update App.jsx

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -2,15 +2,15 @@ const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 }
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14}
     ]
   }
 
   const Header = () => <h1>{course.name}</h1>
 
-  const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+  const Part = ({name, exercises}) => <p>{name} {exercises}</p>
 
   const Content = () => (
     <div>
@@ -22,9 +22,7 @@ const App = () => {
 
   const Total = () => {
     const totalExercises =
-      course.parts[0].exercises +
-      course.parts[1].exercises +
-      course.parts[2].exercises
+      course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
 
     return <p>Total exercises: {totalExercises}</p>
   }
