469a5948bd797fe1d2b7e33355b29060b41ea65a
Deepak Khatri <dk794453@gmail.com>
2026-01-10 19:39:54 +0530

    Delete Part 2/Phonebook directory

--- Part 2/Phonebook/src/filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({search, setSearch}) => {
-  return (
-    <div>
-      filter shown with <input value={search} onChange={e => setSearch(e.target.value)}/>
-    </div>
-  )
-}
-
-export default Filter
--- Part 2/Phonebook/src/persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = ({persons}) => {
-  return (
-    <div>
-      {persons.map(p => (<div key={p.name}>{p.name} {p.number}</div>))}
-    </div>
-  )
-}
-
-export default Persons
